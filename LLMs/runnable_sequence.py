f04ea7f7ee40fd7b631bd08663d009a3361a6541
JayanthBhansali <62511293+JayanthBhansali@users.noreply.github.com>
2025-05-10 16:02:59 -0400

    runnable_sequence

---
+++ LLMs/runnable_sequence.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation",
+)
+
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template = 'Generate a GenZ joke on {topic}',
+    input_variables = ['topic']
+)
+
+
+parser = StrOutputParser()
+
+
+chain = RunnableSequence(prompt, model, parser)
+
+res = chain.invoke({'topic':'AI'})
+print(res)
+
+
